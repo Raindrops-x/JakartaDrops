5667ee6e306bfb6242fd19aa743c626212be3d6b
Raindrops-x <raindrops.bsb@gmail.com>
2025-02-01 15:09:25 -0500

    Componente de Loggin creado

--- src/main/java/com/rain/jdee/util/LoginUtil.java
+++ src/main/java/com/rain/jdee/util/LoginUtil.java
@@ -1,4 +1,31 @@
 package com.rain.jdee.util;
 
+import com.rain.jdee.model.Usuario;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.Optional;
+
 public class LoginUtil {
+    public Optional<Usuario> validarUsuario(HttpServletRequest req){
+        HttpSession sesion = req.getSession();
+        if(sesion != null){
+            Usuario usuario = (Usuario) req.getAttribute("usuario");
+            return Optional.ofNullable(usuario);
+        }
+        return Optional.empty();
+    }
+
+    public void activarSesionUsuario(HttpServletRequest req, Usuario usuario){
+        HttpSession sesion = req.getSession(false);
+        sesion.setAttribute("usuario",usuario);
+    }
+
+    public void cerrarSesionUsuario(HttpServletRequest req){
+        HttpSession sesion = req.getSession(false);
+        if(sesion != null){
+            sesion.removeAttribute("usuario");
+            sesion.invalidate();
+        }
+    }
 }
